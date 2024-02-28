f5530a48bb21dde166f1eb7386cac37833d28116
Marwa Abdelgawad <abdelgawad.m@outlook.de>
2024-02-28 20:58:51 +0100

    Dealor

--- program/application/Main.java
+++ program/application/Main.java
@@ -2,6 +2,14 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Deal or no Deal?");
+        System.out.println("$$$$$$$$$$$$$$$ Welcome to Deal or no Deal $$$$$$$$$$$$$$$");
+        DealOrNoDeal game = new DealOrNoDeal();
+        game.initGame();
+        game.chooseInitialCase();
+        for (int round = 1; round <= 9; round++) {
+            game.playRound(round);
+        }
+        game.finalStage();
     }
+
 }
